998bc2744f2ff7e4d52a686933241de5cb262b90
Rethna Pulikkoonattu <rethna@broadcom.com>
2022-01-13 12:04:51 -0800

    Updated source file

--- src/MyFunRep.jl
+++ src/MyFunRep.jl
@@ -1,5 +1,21 @@
 module MyFunRep
 
 # Write your package code here.
+"""
+A simple add method for testing
+    ```julia> my_fun1(2,3)
+    ```
+"""
+my_fun1(x,y) = x+y 
+
+"""
+A sample function as an experiment
+    ```julia> my_fun2(1,2,3)`
+"""
+function my_fun2(x,y,z)
+    return x+y+z
+end
+
+export my_fun1, my_fun2
 
 end
