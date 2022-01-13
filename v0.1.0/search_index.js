41fffa358575490f40593fef01cac28fbb074d9f
Documenter.jl <documenter@juliadocs.github.io>
2022-01-13 20:04:52 +0000

    build based on 9e0e42d

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyFunRep","category":"page"},{"location":"#MyFunRep","page":"Home","title":"MyFunRep","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyFunRep.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyFunRep]","category":"page"}]
+}
